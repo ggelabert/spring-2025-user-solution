8a347044196b38206806424624d5d105127fdad0
Guillem Gelabert <guillem.gelabert@gmail.com>
2025-06-04 11:29:45 +0200

    Test 1

--- src/test/java/edu/uoc/epcsd/user/domain/service/DigitalSessionServiceUnitTest.java
+++ src/test/java/edu/uoc/epcsd/user/domain/service/DigitalSessionServiceUnitTest.java
@@ -2,9 +2,9 @@ package edu.uoc.epcsd.user.domain.service;
 
 import edu.uoc.epcsd.user.domain.DigitalSession;
 import edu.uoc.epcsd.user.domain.User;
+import edu.uoc.epcsd.user.domain.exception.UserNotFoundException;
 import edu.uoc.epcsd.user.domain.repository.DigitalSessionRepository;
 import edu.uoc.epcsd.user.domain.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,6 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.longThat;
 import static org.mockito.Mockito.when;
 
@@ -42,6 +44,14 @@ public class DigitalSessionServiceUnitTest {
         when(digitalSessionRepository.findDigitalSessionByUser(longThat(i -> i == 1L))).thenReturn(digitalSessions);
         DigitalSessionService service = new DigitalSessionServiceImpl(digitalSessionRepository, userRepository);
         var sessions = service.findDigitalSessionByUser(1L);
-        Assertions.assertEquals(3, sessions.size());
+        assertEquals(3, sessions.size());
+    }
+
+    @Test
+    void findDigitalSessionByUserIdReturnsExceptionIfUserNotFound() {
+        when(userRepository.findUserById(longThat(i -> i == 1L))).thenReturn(Optional.empty());
+        DigitalSessionService service = new DigitalSessionServiceImpl(digitalSessionRepository, userRepository);
+        var exception = assertThrows(UserNotFoundException.class, () -> service.findDigitalSessionByUser(1L));
+        assertEquals("User with id '1' not found", exception.getMessage());
     }
 }
