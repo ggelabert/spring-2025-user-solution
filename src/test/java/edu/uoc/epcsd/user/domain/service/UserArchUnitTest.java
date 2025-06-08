7bb6db6f8df8f9a1faa982a29b7de90dd5ad77ad
Guillem Gelabert <guillem.gelabert@gmail.com>
2025-06-08 20:12:29 +0200

    Test 1

--- src/test/java/edu/uoc/epcsd/user/domain/service/UserArchUnitTest.java
+++ src/test/java/edu/uoc/epcsd/user/domain/service/UserArchUnitTest.java
@@ -1,7 +1,7 @@
 package edu.uoc.epcsd.user.domain.service;
 
 import com.tngtech.archunit.base.DescribedPredicate;
-import com.tngtech.archunit.core.domain.Dependency;
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.properties.HasName;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -9,13 +9,7 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import edu.uoc.epcsd.user.application.rest.response.GetUserResponse;
-import edu.uoc.epcsd.user.domain.*;
-import edu.uoc.epcsd.user.domain.DigitalItem;
-import edu.uoc.epcsd.user.domain.DigitalSession;
-import edu.uoc.epcsd.user.infrastructure.repository.jpa.AlertEntity;
-import edu.uoc.epcsd.user.infrastructure.repository.jpa.DigitalItemEntity;
-import edu.uoc.epcsd.user.infrastructure.repository.jpa.DigitalSessionEntity;
-import edu.uoc.epcsd.user.infrastructure.repository.jpa.UserEntity;
+import edu.uoc.epcsd.user.domain.User;
 import org.springframework.stereotype.Service;
 
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
@@ -24,22 +18,19 @@ import static com.tngtech.archunit.library.Architectures.onionArchitecture;
         , importOptions = ImportOption.DoNotIncludeTests.class)
 public class UserArchUnitTest {
 
-    private static final DescribedPredicate<Dependency> implementsDomainTranslatable = DescribedPredicate.describe("implements DomainTranslatable", dependency -> {
-        return dependency.getOriginClass().getInterfaces().stream().anyMatch(type -> type.equals(HasName.Predicates.name("DomainTranslatable")));
-    });
-
-    private static final DescribedPredicate<Dependency> isModelClass = DescribedPredicate.describe("is from domain model", dependency -> {
-        return dependency.getTargetClass().getPackage().getRelativeName().equals("domain");
-    });
-
     @ArchTest
     static final ArchRule onionArchitectureIsRespected =
             onionArchitecture()
                     .domainModels("..domain")
                     .domainServices("..domain.service", "..domain.repository")
+                    // Tot i que no tenim application services en el nostre cas, ja que és una aplicació molt petita
+                    // afegim els serveis de domini per cumplir la onion architecture
                     .applicationServices("..domain.service")
                     .adapter("rest", "..application.rest")
                     .adapter("persistence", "..infrastructure.repository.jpa", "..infrastructure.repository.rest")
+                    // En aquest cas s'està incomplint l'onion architecture estrictament, però hi ha casos on es permeten
+                    // aquest tipus de dependències, com per exemple per traduïr de Dominio a un adaptador
+                    // donat que no volem modificar el codi font, ignorem la dependència.
                     .ignoreDependency(GetUserResponse.class, User.class);
 
     @ArchTest
